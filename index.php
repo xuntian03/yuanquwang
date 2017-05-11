ba9fee506dc1d806b53d7b24e1e760ec846cf1b9
xuntian03 <516579976@qq.com>
2017-05-11 20:38:14 +0800

    提交index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+define('APP_DEBUG',true);
+define('SITE_URL','http://localhost:88/web/yiqi');
+define('CSS_URL','/web/yiqi/Public/css');
+define('JS_URL','/web/yiqi/Public/js');
+define('IMG_URL','/web/yiqi/Public/img');
+require "./../ThinkPHP/ThinkPHP.php";
